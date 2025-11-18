0f4e879d13e360be3660332db9eb27a7c969d347
JoroGB <134667974+JoroGB@users.noreply.github.com>
2025-11-18 01:11:28 -0600

    set default hotkeys niri

--- desktop/niri_custom.nix
+++ desktop/niri_custom.nix
@@ -85,20 +85,39 @@
 
         # Ventanas
         "Mod+Q".action = close-window;
-        "Mod+F".action = fullscreen-window;
-        "Mod+M".action = maximize-column;
+        # "Mod+F".action = fullscreen-window;
+        # "Mod+M".action = maximize-column;
+        "Mod+BracketLeft".action  = consume-or-expel-window-left;
+        "Mod+BracketRight".action = consume-or-expel-window-right;
+        "Mod+Ctrl+B".action  =  expel-window-from-column;
+
 
         # Navegación
-        "Mod+H".action = focus-column-left;
-        "Mod+L".action = focus-column-right;
-        "Mod+J".action = focus-window-down;
-        "Mod+K".action = focus-window-up;
+
+       "Mod+Left".action = focus-column-left;
+       "Mod+Down".action = focus-window-down;
+       "Mod+Up".action   = focus-window-up;
+       "Mod+Right".action= focus-column-right;
+       "Mod+H".action    = focus-column-left;
+       "Mod+J".action    = focus-window-down;
+       "Mod+K".action    = focus-window-up;
+       "Mod+L".action    = focus-column-right;
+
+       "Mod+Shift+Left".action = move-column-left;
+       "Mod+Shift+Down".action = move-window-down;
+       "Mod+Shift+Up".action   = move-window-up;
+       "Mod+Shift+Right".action= move-column-right;
+       "Mod+Shift+H".action    = move-column-left;
+       "Mod+Shift+J".action    = move-window-down;
+       "Mod+Shift+K".action    = move-window-up;
+       "Mod+Shift+L".action    = move-column-right;
+
 
         # Mover ventanas
-        "Mod+Shift+H".action = move-column-left;
-        "Mod+Shift+L".action = move-column-right;
-        "Mod+Shift+J".action = move-window-down;
-        "Mod+Shift+K".action = move-window-up;
+        "Mod+Ctrl+H".action = move-column-to-monitor-left;
+        "Mod+Ctrl+L".action = move-column-to-monitor-right;
+        # "Mod+Shift+J".action = move-window-down-or-to-workspace-down;
+        # "Mod+Shift+K".action = move-window-up-or-to-workspace-up;
 
         # Workspaces
         "Mod+1".action = focus-workspace 1;
@@ -106,15 +125,83 @@
         "Mod+3".action = focus-workspace 3;
         "Mod+4".action = focus-workspace 4;
 
-        # # Mover a workspace
-        # "Mod+Shift+1".action = move-column-to-workspace 1;
-        # "Mod+Shift+2".action = move-column-to-workspace 2;
-        # "Mod+Shift+3".action = move-column-to-workspace 3;
-        # "Mod+Shift+4".action = move-column-to-workspace 4;
+        #Monitores
+        "Mod+Alt+H".action = focus-monitor-left;
+        "Mod+Alt+L".action = focus-monitor-right;
 
-        # Sistema
-        "Mod+Shift+E".action = quit;
-        "Mod+Shift+R".action = spawn "sh" "-c" "niri msg action quit; niri";
+       "Mod+Shift+Ctrl+Left".action  = move-column-to-monitor-left;
+       "Mod+Shift+Ctrl+Down".action  = move-column-to-monitor-down;
+       "Mod+Shift+Ctrl+Up".action    = move-column-to-monitor-up;
+       "Mod+Shift+Ctrl+Right".action = move-column-to-monitor-right;
+       "Mod+Shift+Ctrl+H".action     = move-column-to-monitor-left;
+       "Mod+Shift+Ctrl+J".action     = move-column-to-monitor-down;
+       "Mod+Shift+Ctrl+K".action     = move-column-to-monitor-up;
+       "Mod+Shift+Ctrl+L".action     = move-column-to-monitor-right;
+
+
+         # Mover a workspace
+        "Mod+Page_Down".action     = focus-workspace-down;
+        "Mod+Page_Up".action       = focus-workspace-up;
+        "Mod+U".action             = focus-workspace-down;
+        "Mod+I".action             = focus-workspace-up;
+        "Mod+Ctrl+Page_Down".action= move-column-to-workspace-down;
+        "Mod+Ctrl+Page_Up".action  = move-column-to-workspace-up;
+        "Mod+Ctrl+U".action        = move-column-to-workspace-down;
+        "Mod+Ctrl+I".action        = move-column-to-workspace-up;
+
+        "Mod+Shift+Page_Down".action = move-workspace-down;
+        "Mod+Shift+Page_Up".action   = move-workspace-up;
+        "Mod+Shift+U".action         = move-workspace-down;
+        "Mod+Shift+I".action         = move-workspace-up;
+
+        # expel
+        "Mod+Comma".action = consume-window-into-column;
+        "Mod+Period".action = expel-window-from-column;
+        "Mod+R".action = switch-preset-column-width;
+
+        "Mod+Shift+R".action = switch-preset-window-height;
+        "Mod+Ctrl+R".action  = reset-window-height;
+        "Mod+F".action       = maximize-column;
+        "Mod+Shift+F".action = fullscreen-window;
+
+        #Expand focused column
+        "Mod+Ctrl+F".action =  expand-column-to-available-width;
+
+        "Mod+C".action = center-column;
+        "Mod+Ctrl+C".action = center-visible-columns;
+
+        "Mod+Minus".action.set-column-width = "-10%";
+        "Mod+Equal".action.set-column-width = "+10%";
+        "Mod+Shift+Minus".action.set-window-height = "-10%";
+        "Mod+Shift+Equal".action.set-window-height = "+10%";
+
+      #Move the focused window between the floating and the tiling layout.
+      "Mod+V".action       = toggle-window-floating;
+      "Mod+Shift+V".action = switch-focus-between-floating-and-tiling;
+
+      # Toggle tabbed column display mode.
+      # Windows in this column will appear as vertical tabs,
+      # rather than stacked on top of each other.
+      "Mod+W".action = toggle-column-tabbed-display;
+
+
+       # ERROR
+       # "Print".action = screenshot;
+       # "Ctrl+Print".action = screenshot-screen;
+       # "Alt+Print".action = screenshot-window;
+
+
+      "Ctrl+Alt+Delete".action =   quit;
+
+       # Powers off the monitors. To turn them back on, do any input like
+       # moving the mouse or pressing any other key.
+      "Mod+Shift+P".action  = power-off-monitors;
+
+      # Sistema
+      "Mod+Shift+E".action = quit;
+        # "Mod+Shift+R".action = spawn "sh" "-c" "niri msg action quit; niri";
+
+        #  Finer height adjustments when in column with other windows.
       };
 
       # Ventanas flotantes
